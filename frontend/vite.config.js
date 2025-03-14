7c3c91fd927483620db001641dad4cdd3f074ef9
online shop <dove.sh@yandex.ru>
2025-03-14 15:35:14 +0300

    vite proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,17 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  root: path.resolve(__dirname ),
-  build: {
-    outDir: path.resolve(__dirname, 'dist'),
-  },
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  build: {
+      outDir: 'dist',
+  },
 });
