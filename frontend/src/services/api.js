b5da3f8a95537294c1c15af8e4819560216bc21f
online shop <dove.sh@yandex.ru>
2025-03-14 15:17:35 +0300

    настройка окружения

---
+++ frontend/src/services/api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const api_base_url = 'http://localhost:5001';
+
+const api = axios.create({
+    base_url: api_base_url,
+});
+
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+    console.error("API Error:", error);
+    return Promise.reject(error);
+    }
+);
+
+export default api;
