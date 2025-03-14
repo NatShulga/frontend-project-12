d3bd2d4f260538ce5df912d8452ac10694341d2c
online shop <dove.sh@yandex.ru>
2025-03-14 16:35:40 +0300

    удаление из окружения

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,15 +4,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 5002,
     proxy: {
       '/api': {
         target: 'http://localhost:5001',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  },
-  build: {
-      outDir: 'dist',
+      },
+      '/socket.io': {
+        target: 'ws://localhost:5001',
+        ws: true,
+        rewriteWsOrigin: true,
+      },
+    },
   },
 });
