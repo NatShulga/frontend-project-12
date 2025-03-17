5f883291767210e5434d4b90102b03b65bd6fc0a
online shop <dove.sh@yandex.ru>
2025-03-17 17:00:30 +0300

    дебаг сборки

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,27 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: path.resolve(__dirname, 'frontend'),
   server: {
     port: 5002,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5001',
+      "/api": {
+        target: "http://localhost:5001",
       },
-      '/socket.io': {
-        target: 'ws://localhost:5001',
+      "/socket.io": {
+        target: "ws://localhost:5001",
         ws: true,
         rewriteWsOrigin: true,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'frontend/index.html'),
       },
     },
   },
