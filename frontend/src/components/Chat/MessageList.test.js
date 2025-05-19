f0b50646d8ab91b8c021bfda85c1394bf1d1cc85
online shop <dove.sh@yandex.ru>
2025-05-19 15:56:01 +0300

    тестирование фильтра нецензурных слов

--- frontend/src/components/Chat/MessageList.test.js
+++ frontend/src/components/Chat/MessageList.test.js
@@ -6,6 +6,6 @@ test('фильтрация нецензурных слов', () => {
   expect(cleanText('Это тестовое сообщение')).toBe('Это тестовое сообщение');
   expect(cleanText('Это хреновое сообщение')).toBe('Это **** сообщение');
   
-  expect(cleanText('п@зда')).toBe('****');
-  expect(cleanText('пиzда')).toBe('****');
+  expect(cleanText('fuck')).toBe('****');
+  expect(cleanText('shit')).toBe('****');
 });
