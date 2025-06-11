1dc4beb7e1b2420ac16e2a0c7c6f35f18bc9c83a
NatShulga <mrs.flowerfields@gmail.com>
2025-06-11 13:14:42 +0300

    навбар откат

--- frontend/src/components/header/Navbar.jsx
+++ frontend/src/components/header/Navbar.jsx
@@ -2,22 +2,17 @@ import React from 'react';
 import { useNavigate } from "react-router-dom";
 import { useTranslation } from 'react-i18next';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
-import { toast } from 'react-toastify'; 
-import { useDispatch } from 'react-redux';
 
 const navBar = () => {
     const { t } = useTranslation();
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
-    const dispatch = useDispatch();
 
-  
-    const logout = async () => {
+
+    const logout = () => {
         try {
-          await dispatch(resetStore());
-          localStorage.removeItem('token');
+          localStorage.removeItem("token"); // Удаляем только токен
         navigate("/login");
-        toast.success(t("Вы успешно вышли"));
         } catch (error) {
         console.error("Ошибка при выходе:", error);
         }
